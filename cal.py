ef789e9be86149c7db080502a1aa3653e4c6b17d
LUUTU JOSEPH(Shelby Gamers Ug) <107415914+miirotrevor@users.noreply.github.com>
2023-07-31 15:57:29 +0300

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,10 @@
+def add_numbers(a, b):
+    return a + b
+
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum and display the result
+result = add_numbers(num1, num2)
+print("The sum is:", result)
